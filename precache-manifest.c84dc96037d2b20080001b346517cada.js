c03582e5e0230ff3b52ceacb96420125a11ae98e
Albert <albertmik1915@gmail.com>
2020-04-23 19:30:18 +0300

    Updates

---
+++ precache-manifest.c84dc96037d2b20080001b346517cada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6689644c3970cc06e8331055f5e090",
+    "url": "/service.v1.0.1/index.html"
+  },
+  {
+    "revision": "7e1ec945a87caf5ebe88",
+    "url": "/service.v1.0.1/static/css/2.abebce0a.chunk.css"
+  },
+  {
+    "revision": "7e1ec945a87caf5ebe88",
+    "url": "/service.v1.0.1/static/js/2.f41cfa66.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/service.v1.0.1/static/js/2.f41cfa66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d406cd22d8d8528fbcf",
+    "url": "/service.v1.0.1/static/js/main.a2c6074d.chunk.js"
+  },
+  {
+    "revision": "233b4ce4a551c2f28762",
+    "url": "/service.v1.0.1/static/js/runtime-main.216f619b.js"
+  }
+]);
